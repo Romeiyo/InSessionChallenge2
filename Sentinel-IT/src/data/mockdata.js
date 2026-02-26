e99e0c5e95a4ba8a78212c66c04bbf82b88d828f
Romeiyo <romeopomeo1@gmail.com>
2026-02-26 13:04:08 +0200

    InSession Assignment

---
+++ Sentinel-IT/src/data/mockdata.js
@@ -0,0 +1,7 @@
+const initialTickets = [
+    { id:1, employeeName: 'John Cena', date: '2026-02-27', status: 'Pending', issue:'No Network', priority: 'High'},
+    { id:2, employeeName: 'Randy Orton', date: '2026-02-28', status: 'Pending', issue:'Blank Screen', priority: 'Medium'},
+    { id:3, employeeName: 'Rhea Ripley', date: '2026-02-26', status: 'Pending', issue:'Can not Login', priority: 'High'},
+];
+
+export default initialTickets
