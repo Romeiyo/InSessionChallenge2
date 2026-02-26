a7047f6db0d585e7b3613761ed88c4a28c0aa64b
Romeiyo <romeopomeo1@gmail.com>
2026-02-26 13:20:27 +0200

    InSession Assignment, 2nd commit

--- Sentinel-IT/src/App.jsx
+++ Sentinel-IT/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import Layout from './components/Layout';
+import TicketForm from './components/TicketForm';
+import TicketList from './components/TicketList';
+import {useTickets} from './hooks/useTickets';
 
 function App() {
+  const { tickets, isLoading, addTicket } = useTickets();
 
   return (
-    <>
-      
-    </>
+    <Layout>
+      <TicketForm onAdd={addTicket}/>
+
+      {isLoading ? (
+        <p className="loading">Fetching history...</p>
+      ) : (
+        <TicketList tickets={tickets} />
+      )}
+    </Layout>
   )
 }
 
